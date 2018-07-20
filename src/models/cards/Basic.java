5f59e4088b2034a8531b7e730f61485650a45a04
Siraj Ahmadzai <siraj_ahmadzai@hotmail.com>
2018-07-19 20:57:09 -0400

    added rank to constructor removed rank attribute removed rank set in constructor

---
+++ src/models/cards/Basic.java
@@ -0,0 +1,13 @@
+package models.cards;
+
+import models.Suit;
+
+public class Basic extends Card {
+
+    public Basic(Suit suit, int pointValue, String name, int rank) {
+        super(suit, pointValue, name, rank);
+    }
+
+
+
+}
