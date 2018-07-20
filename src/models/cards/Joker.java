f8c4fbf57e3055e91790d92d240a482b08a95192
Siraj Ahmadzai <siraj_ahmadzai@hotmail.com>
2018-07-19 20:55:19 -0400

    added change rank method override changed parent to Changeable removed rank attribute added rank to constructors

---
+++ src/models/cards/Joker.java
@@ -0,0 +1,20 @@
+package models.cards;
+
+import models.Suit;
+
+public class Joker extends Changeable {
+
+    public Joker(Suit suit, int pointValue, String name, int rank) {
+        super(suit, pointValue, name,rank);
+    }
+
+
+    @Override
+    public void changeRank(int rank) throws InvalidCardRankException {
+        if (rank < 1 || rank > 14) {
+            throw new InvalidCardRankException();
+        } else {
+            this.rank = rank;
+        }
+    }
+}
