acc78d1cf7ef10ffc3790876d9efdeca0c68dd4d
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-07-23 22:30:22 -0400

    moved deck with methods to the table created machiavelliDeck shuffled deck requested input from user for number of players created players dealt cards to players

--- src/models/Machiavelli.java
+++ src/models/Machiavelli.java
@@ -1,22 +1,37 @@
 package models;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import models.cards.Basic;
 import models.cards.Card;
+import models.cards.Joker;
+import models.cards.Suit;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Machiavelli {
 
-    private ArrayList<Card> deck;
+
     private Table table;
+    private ArrayList<Player> players;
 
-    public void initDeck() {
+
+    public Machiavelli(int numOfPlayers) {
+        players = new ArrayList<>();
+        table = new Table();
+
+        for (int playerCounter = 0; playerCounter < numOfPlayers; playerCounter++) {
+            players.add(new Player(numOfPlayers, "Player" + numOfPlayers + 1));
+        }
+
+        dealHands();
+
     }
 
-    public void shuffleDeck() {
-
-    }
 
     public void mergeSet() {
+
     }
 
     public void splitSet(int pos) {
@@ -31,6 +46,31 @@ public class Machiavelli {
     }
 
     public void appendCard(Card card) {
+
     }
 
+    private void verifyTable() {
+
+    }
+
+    private void dealHands() {
+        int currPlayerID = (new Random()).nextInt(players.size());
+
+        for (int cardCounter = 0; cardCounter < 15; cardCounter++) {
+
+            int playerCounter = 0;
+
+            while (playerCounter < players.size()) {
+
+                if (currPlayerID >= players.size()) {
+
+                    currPlayerID = 0;
+                }
+                players.get(currPlayerID).getHand().add(table.getDeck().remove(table.getDeck().size() - 1));
+
+                currPlayerID++;
+                playerCounter++;
+            }
+        }
+    }
 }
--- src/models/Player.java
+++ src/models/Player.java
@@ -8,6 +8,20 @@ public class Player {
 
     private int playerID;
     private String name;
+
+
+
     private ArrayList<Card> hand;
     private int pointValue;
+
+    public Player(int playerID, String name) {
+        this.playerID = playerID;
+        this.name = name;
+        hand = new ArrayList<>();
+        pointValue = 0;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
 }
