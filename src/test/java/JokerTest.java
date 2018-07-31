8950c68364a6c8a72066153e6ad90f5fc6e499f1
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-07-30 20:41:38 -0400

    added test classes

---
+++ src/test/java/JokerTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+public class JokerTest extends TestCase {
+
+    public void testChangeRank(){
+
+    }
+}
---
+++ src/test/java/MachaivelliTest.java
@@ -0,0 +1,49 @@
+import junit.framework.TestCase;
+
+public class MachaivelliTest extends TestCase {
+
+    public void testDrawCardFromDeck(){
+
+    }
+
+    public void testPlayCard(){
+
+    }
+
+    public void testPlayCardFromPlayArea(){
+
+    }
+
+    public void testMergeSet(){
+
+    }
+
+    public void testSplitSet(){
+
+    }
+
+    public void testSplitSetRemove(){
+
+    }
+
+    public void testPrependCard(){
+
+    }
+
+    public void testAppendCard(){
+
+    }
+
+    public void testVerifyTable(){
+
+    }
+
+    public void testDealHands(){
+
+    }
+
+    public void testPickPlayerTurn(){
+        
+    }
+
+}
