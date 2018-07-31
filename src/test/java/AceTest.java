804bd33cec62b9b4a0ae689de2510b7ecc5edcb0
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-07-30 22:14:41 -0400

    more tests

--- src/test/java/AceTest.java
+++ src/test/java/AceTest.java
@@ -3,5 +3,12 @@ import junit.framework.TestCase;
 
 public class AceTest extends TestCase {
 
+    public void testAce(){
+
+    }
+
+    public void testChangeRank(){
+
+    }
 
 }
--- src/test/java/JokerTest.java
+++ src/test/java/JokerTest.java
@@ -1,6 +1,22 @@
 import junit.framework.TestCase;
+import models.cards.Joker;
+import models.cards.Suit;
 
 public class JokerTest extends TestCase {
+
+    public void testJoker(){
+        Suit suit = Suit.JOKER;
+        int pointValue = 20;
+        String name = "Joker";
+        int rank = 15;
+
+        Joker joker = new Joker(suit,20,name,rank);
+
+        assertEquals(suit, joker.getSuit());
+        assertEquals(pointValue, joker.getPointValue());
+        assertEquals(name, joker.getName());
+        assertEquals(rank, joker.getRank());
+    }
 
     public void testChangeRank(){
 
