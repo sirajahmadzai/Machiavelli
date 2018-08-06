7f0eee689768ee5c26368aba5b281a431a2ce2cb
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-06 16:39:43 -0400

    done

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,24 +1,68 @@
-import junit.framework.TestCase;
-import models.cards.Basic;
-import models.cards.Suit;
-
-public class BasicTest extends TestCase {
-
-    /**
-     * tests construction of Basic card object
-     */
-    public void testBasic() {
-        final Suit INITIAL_SUIT = Suit.CLUBS;
-        final int INITIAL_POINT_VALUE = 5;
-        final String INITIAL_NAME = "5";
-        final int INITIAL_RANK = 5;
-
-        final Basic BASIC_CARD = new Basic(INITIAL_SUIT, INITIAL_POINT_VALUE, INITIAL_NAME, INITIAL_RANK, -1);
-
-        assertEquals(INITIAL_SUIT, BASIC_CARD.getSuit());
-        assertEquals(INITIAL_POINT_VALUE, BASIC_CARD.getPointValue());
-        assertEquals(INITIAL_NAME, BASIC_CARD.getName());
-        assertEquals(INITIAL_RANK, BASIC_CARD.getRank());
-    }
-
-}
+//import com.sun.javaws.exceptions.InvalidArgumentException;
+//import junit.framework.TestCase;
+//import models.cards.Basic;
+//import models.cards.Card;
+//import models.cards.Suit;
+//
+//public class BasicTest extends TestCase {
+//
+//    /**
+//     * tests construction of Basic card object
+//     */
+//    public void testBasic() {
+//        final Suit INITIAL_SUIT = Suit.CLUBS;
+//        final int INITIAL_POINT_VALUE = 5;
+//        final String INITIAL_NAME = "5";
+//        final int INITIAL_RANK = 5;
+//
+//        final Basic BASIC_CARD = new Basic(INITIAL_SUIT, INITIAL_POINT_VALUE, INITIAL_NAME, INITIAL_RANK, -1);
+//
+//        assertEquals(INITIAL_SUIT, BASIC_CARD.getSuit());
+//        assertEquals(INITIAL_POINT_VALUE, BASIC_CARD.getPointValue());
+//        assertEquals(INITIAL_NAME, BASIC_CARD.getName());
+//        assertEquals(INITIAL_RANK, BASIC_CARD.getRank());
+//    }
+//
+//    public void testGetCardNameByRank() {
+//
+//        final int INITIAL_RANK = 5;
+//
+//
+//        final Card INITIAL_CARD = new Basic(Suit.CLUBS, 5, "5", 5, 0);
+//        //cant make this one a constant
+//        String INITIAL_CARD_NAME = null;
+//
+//        try {
+//            INITIAL_CARD_NAME = INITIAL_CARD.getCardNameByRank(INITIAL_RANK);
+//        } catch (InvalidArgumentException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            assertEquals(INITIAL_CARD_NAME, INITIAL_CARD.getCardNameByRank(INITIAL_RANK));
+//        } catch (InvalidArgumentException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    public void testGetCardValueByRank() {
+//        //cant make this one final
+//        int INITIAL_RANK = 0;
+//
+//        final Card INITIAL_CARD = new Basic(Suit.SPADES, 5, "5", 5, 1);
+//
+//        try {
+//            INITIAL_RANK = INITIAL_CARD.getCardValueByRank(INITIAL_RANK);
+//        } catch (InvalidArgumentException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+////            assertEquals(INITIAL_RANK, INITIAL_CARD.getCardValueByRank(INITIAL_RANK));
+//        } catch (InvalidArgumentException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//}
---
+++ src/test/java/models/cards/JokerTest.java
@@ -0,0 +1,66 @@
+package models.cards;
+
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JokerTest {
+
+    @Test
+    public void testConstructor(){
+        final Suit INITIAL_SUIT = Suit.JOKER;
+        final int INITIAL_RANK = 15;
+        final int INITIAL_ID = 1;
+
+
+        try {
+            final Joker JOKER_CARD = new Joker(INITIAL_SUIT, INITIAL_RANK, -1);
+            final int EXPECTED_POINT_VALUE = 20;
+            final String EXPECTED_NAME = "Joker";
+
+
+            assertEquals("getSuit() != INITIAL_SUIT",INITIAL_SUIT, JOKER_CARD.getSuit());
+            assertEquals("getPointValue() != EXPECTED_POINT_VALUE", EXPECTED_POINT_VALUE, JOKER_CARD.getPointValue());
+            assertEquals("getName() != EXPECTED_NAME",EXPECTED_NAME, JOKER_CARD.getName());
+            assertEquals("getRank() != INITIAL_RANK",INITIAL_RANK, JOKER_CARD.getRank());
+            assertEquals("getId() != INITIAL_ID",INITIAL_ID, JOKER_CARD.getId());
+        } catch (InvalidArgumentException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Test
+    public void changeRank() {
+        final Suit INITIAL_SUIT = Suit.JOKER;
+        final int INITIAL_RANK = 15;
+        final int INITIAL_ID = 1;
+
+        try {
+            final Joker INITIAL_JOKER_CARD = new Joker(INITIAL_SUIT, INITIAL_RANK, INITIAL_ID);
+
+            //VALID rank
+            try {
+                final int VALID_RANK = 2;
+                INITIAL_JOKER_CARD.changeRank(VALID_RANK);
+
+                assertEquals("getRank != VALID_RANK", VALID_RANK, INITIAL_JOKER_CARD.getRank());
+            } catch (Card.InvalidCardRankException e) {
+                fail("Unexpected Exception");
+            }
+
+            //INVALID RANK
+
+            try {
+                final int INVALID_RANK = -2;
+                INITIAL_JOKER_CARD.changeRank(INVALID_RANK);
+
+                fail("Invalid rank did not throw an exception");
+            } catch (Card.InvalidCardRankException e) {
+                //passed
+            }
+        } catch (InvalidArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
