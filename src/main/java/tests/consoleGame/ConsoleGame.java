c208868c311d17dfbcf181f3e0c498d25f64f679
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-13 00:59:31 -0400

    printed currPlayer and currPlayer's hand

--- src/main/java/tests/consoleGame/ConsoleGame.java
+++ src/main/java/tests/consoleGame/ConsoleGame.java
@@ -1,6 +1,8 @@
 package tests.consoleGame;
 
 import models.Machiavelli;
+import models.Player;
+import models.cards.Card;
 
 import java.util.Scanner;
 
@@ -18,6 +20,18 @@ public class ConsoleGame {
 
                 if (numOfPlayers < 2 || numOfPlayers > 4) {
                     System.out.println("Error number out of range!");
+                }
+                Machiavelli game = new Machiavelli(numOfPlayers);
+                Player currPlayer = game.getRandomPlayer();
+                System.out.println("current player is: " + currPlayer.getName());
+                System.out.println("currPlayer hand is:");
+                int cardCounter = 0;
+                for (Card card : currPlayer.getHand()) {
+                    System.out.println("card" + cardCounter);
+                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getSuit());
+                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getRank());
+                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getId());
+                    cardCounter++;
                 }
             } catch (NumberFormatException e) {
 
