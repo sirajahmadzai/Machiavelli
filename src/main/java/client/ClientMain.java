0c07955d6f0d03888bd50430a15b6c6ee911f0fe
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-16 16:33:44 -0400

    added ClientMain

---
+++ src/main/java/client/ClientMain.java
@@ -0,0 +1,14 @@
+package client;
+
+import javafx.application.Application;
+
+import java.io.IOException;
+
+
+public class ClientMain {
+
+    public static void main(String[] args) throws IOException {
+        Application.launch(App.class);
+    }
+}
+
