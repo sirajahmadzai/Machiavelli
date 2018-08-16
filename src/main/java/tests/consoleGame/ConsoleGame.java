975e9ad35bc9ee327356a8de1e6df2d7baa5f776
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-16 15:32:53 -0400

    started adding logic to ConsoleGame

--- src/main/java/tests/consoleGame/ConsoleGame.java
+++ src/main/java/tests/consoleGame/ConsoleGame.java
@@ -23,71 +23,84 @@ public class ConsoleGame {
                 if (numOfPlayers < 2 || numOfPlayers > 4) {
                     System.out.println("Error number out of range!");
                 }
+                else {
 
-                Machiavelli game = new Machiavelli(numOfPlayers);
-                System.out.println("Print players with specifications");
-                int playerCounter = 1;
-                for (Player player : game.getPlayers()) {
-                    System.out.println("player" + playerCounter + "'s playerID: " + player.getPlayerID());
-                    System.out.println("player" + playerCounter + "'s name: " + player.getName());
-                    System.out.println("player" + playerCounter + "'s pointValue: " + player.getPointValue());
-                    System.out.println("player" + playerCounter + "'s hand: ");
-                    playerCounter++;
-                    int cardCounter = 1;
-                    for (Card card : player.getHand()) {
-                        System.out.println("card" + cardCounter);
-                        System.out.println("card" + cardCounter + "'s ID: " + card.getId());
-                        System.out.println("card" + cardCounter + "'s rank: " + card.getRank());
-                        System.out.println("card" + cardCounter + "'s Suit: " + card.getSuit());
-                        cardCounter++;
+                    Machiavelli machiavelli = new Machiavelli(numOfPlayers);
+                    System.out.println("time to select a random player to start the game!");
+                    Player randomPlayer = machiavelli.getRandomPlayer();
+                    System.out.println("random player is Player" + randomPlayer.getPlayerID());
 
-                    }
+
+
+                    /**********************
+                     * COMMENTED OUT RANDOM PRINTS OF GAME PARTS
+                     */
+
+//                    Machiavelli game = new Machiavelli(numOfPlayers);
+//                    System.out.println("Print players with specifications");
+//                    int playerCounter = 1;
+//                    for (Player player : game.getPlayers()) {
+//                        System.out.println("player" + playerCounter + "'s playerID: " + player.getPlayerID());
+//                        System.out.println("player" + playerCounter + "'s name: " + player.getName());
+//                        System.out.println("player" + playerCounter + "'s pointValue: " + player.getPointValue());
+//                        System.out.println("player" + playerCounter + "'s hand: ");
+//                        playerCounter++;
+//                        int cardCounter = 1;
+//                        for (Card card : player.getHand()) {
+//                            System.out.println("card" + cardCounter);
+//                            System.out.println("card" + cardCounter + "'s ID: " + card.getId());
+//                            System.out.println("card" + cardCounter + "'s rank: " + card.getRank());
+//                            System.out.println("card" + cardCounter + "'s Suit: " + card.getSuit());
+//                            cardCounter++;
+//
+//                        }
+//                    }
+//
+//
+//                    Player currPlayer = game.getRandomPlayer();
+//                    System.out.println("current player is: " + currPlayer.getName());
+//                    System.out.println("currPlayer hand is:");
+//                    int currPlayerCardCounter = 1;
+//                    for (Card card : currPlayer.getHand()) {
+//                        System.out.println("card" + currPlayerCardCounter);
+//                        System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getSuit());
+//                        System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getRank());
+//                        System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getId());
+//                        currPlayerCardCounter++;
+//                    }
+//
+//                    Table table = game.getTable();
+//                    int cardSetCounter = 1;
+//                    int cardSetCardCounter = 1;
+//                    for (CardSet cardSet : table.getCardSets()) {
+//                        System.out.println("cardSet" + cardSetCounter + "'s list of cards");
+//                        for (Card card : cardSet.getCards()) {
+//                            System.out.println("card" + cardSetCardCounter + "'s Suit" + card.getSuit());
+//                            System.out.println("card" + cardSetCardCounter + "'s rank" + card.getRank());
+//                            System.out.println("card" + cardSetCardCounter + "'s ID" + card.getId());
+//                            cardSetCardCounter++;
+//                        }
+//                        cardSetCounter++;
+//                    }
+//                    int deckCardCounter = 1;
+//                    for (Card card : table.getDeck()) {
+//                        System.out.println("List of cards in table's deck");
+//                        System.out.println("card" + deckCardCounter + "'s Suit" + card.getSuit());
+//                        System.out.println("card" + deckCardCounter + "'s rank" + card.getRank());
+//                        System.out.println("card" + deckCardCounter + "'s ID" + card.getId());
+//                        deckCardCounter++;
+//                    }
+//
+//                    int cardsInPlayCardCounter = 1;
+//                    for (Card card : table.getCardsInPlay()) {
+//                        System.out.println("List of cards in play");
+//                        System.out.println("card" + cardsInPlayCardCounter + "'s Suit" + card.getSuit());
+//                        System.out.println("card" + cardsInPlayCardCounter + "'s rank" + card.getRank());
+//                        System.out.println("card" + cardsInPlayCardCounter + "'s ID" + card.getId());
+//                        currPlayerCardCounter++;
+//                    }
+
                 }
-
-
-                Player currPlayer = game.getRandomPlayer();
-                System.out.println("current player is: " + currPlayer.getName());
-                System.out.println("currPlayer hand is:");
-                int currPlayerCardCounter = 1;
-                for (Card card : currPlayer.getHand()) {
-                    System.out.println("card" + currPlayerCardCounter);
-                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getSuit());
-                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getRank());
-                    System.out.println(currPlayer.getHand().get(currPlayer.getHand().indexOf(card)).getId());
-                    currPlayerCardCounter++;
-                }
-
-                Table table = game.getTable();
-                int cardSetCounter = 1;
-                int cardSetCardCounter = 1;
-                for (CardSet cardSet : table.getCardSets()) {
-                    System.out.println("cardSet" + cardSetCounter + "'s list of cards");
-                    for (Card card : cardSet.getCards()) {
-                        System.out.println("card" + cardSetCardCounter + "'s Suit" + card.getSuit());
-                        System.out.println("card" + cardSetCardCounter + "'s rank" + card.getRank());
-                        System.out.println("card" + cardSetCardCounter + "'s ID" + card.getId());
-                        cardSetCardCounter++;
-                    }
-                    cardSetCounter++;
-                }
-                int deckCardCounter = 1;
-                for (Card card : table.getDeck()) {
-                    System.out.println("List of cards in table's deck");
-                    System.out.println("card" + deckCardCounter + "'s Suit" + card.getSuit());
-                    System.out.println("card" + deckCardCounter + "'s rank" + card.getRank());
-                    System.out.println("card" + deckCardCounter + "'s ID" + card.getId());
-                    deckCardCounter++;
-                }
-
-                int cardsInPlayCardCounter = 1;
-                for (Card card : table.getCardsInPlay()) {
-                    System.out.println("List of cards in play");
-                    System.out.println("card" + cardsInPlayCardCounter + "'s Suit" + card.getSuit());
-                    System.out.println("card" + cardsInPlayCardCounter + "'s rank" + card.getRank());
-                    System.out.println("card" + cardsInPlayCardCounter + "'s ID" + card.getId());
-                    currPlayerCardCounter++;
-                }
-
             } catch (NumberFormatException e) {
 
                 System.out.println("ERROR! Please enter a number 2-4!");
