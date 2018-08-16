800034b1cc3c0ed51ad8f0666090d29a50a8fef4
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-16 16:35:04 -0400

    added ServerMain class

--- src/main/java/server/ServerMain.java
+++ src/main/java/server/ServerMain.java
@@ -1,4 +1,11 @@
 package server;
 
+/**
+ * FULLY STATIC CLASS THAT STARTS THE PROGRAM OBJECT
+ */
 public class ServerMain {
+    public static void main(String[] args) throws Exception {
+        new Server().start();
+        //new Server("YOUR IP HERE", 9876)).start();
+    }
 }
