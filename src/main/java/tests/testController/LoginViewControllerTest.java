69efedea6f98792600b8d035d5a41f2e3f9c6354
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-20 22:58:53 -0400

    added test controllers

---
+++ src/main/java/tests/testController/LoginViewControllerTest.java
@@ -0,0 +1,34 @@
+package tests.testController;
+
+import client.App;
+import client.views.LoginView;
+
+public class LoginViewControllerTest {
+private App app;
+private LoginView loginView;
+private String ip;
+private int port;
+
+    public LoginViewControllerTest(App app, LoginView loginView) {
+        this.app = app;
+        this.loginView = loginView;
+
+
+        loginView.addBtnLoginAction(e->{
+            String ip = loginView.getIp();
+            int port = loginView.getPort();
+
+            //todo: use this ip and port to connect, if it connects you're connected, if not show an error message.
+
+            if(false){
+
+            }
+            else
+
+            {
+                System.out.println("Test");
+                loginView.getLblMessage().setText("Connection refused check IP and Port!");
+            }
+        });
+    }
+}
