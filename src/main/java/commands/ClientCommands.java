61cc4d8e5912ee2fe1dbf4deb9e26fa14c3fe3cf
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-20 22:52:47 -0400

    added commands

--- src/main/java/commands/ClientCommands.java
+++ src/main/java/commands/ClientCommands.java
@@ -1,4 +1,12 @@
 package commands;
 
 public enum ClientCommands {
+    NUMBER_OF_PLAYERS,
+    ADD_CARD_TO_HAND,
+    DRAW_CARD_FROM_DECK,
+    ADD_CARD_TO_SET,
+    REMOVE_CARD_FROM_HAND,
+    REMOVE_CARD_FROM_SET,
+    DECK_OF_CARDS_CLICKED,
+
 }
