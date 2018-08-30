15eb10fd96824b50e93e60d9ee699508271c6c92
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-29 22:05:09 -0400

    generated constructor

--- src/main/java/server/controllers/Controller.java
+++ src/main/java/server/controllers/Controller.java
@@ -1,6 +1,14 @@
 package server.controllers;
 
+import server.models.Machiavelli;
+import server.models.cards.Card;
+
 public class Controller {
 
+    private Machiavelli machiavelli;
 
+
+    public Controller(Machiavelli machiavelli) {
+        this.machiavelli = machiavelli;
+    }
 }
