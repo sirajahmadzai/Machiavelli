c57605dfc880bde428968888a7e9d75b2b94c29e
sirajahmadzai <siraj_ahmadzai@hotmail.com>
2018-08-29 21:57:02 -0400

    added toString() method

--- src/main/java/server/models/cards/Basic.java
+++ src/main/java/server/models/cards/Basic.java
@@ -14,5 +14,13 @@ public class Basic extends Card {
         super(suit, rank, id);
     }
 
-
+    @Override
+    public String toString() {
+        String rank = "";
+        if (this.rank == 11) rank = "j";
+        else if (this.rank == 12) rank = "q";
+        else if (this.rank == 13) rank = "k";
+        else rank = String.valueOf(this.rank);
+        return rank + this.getSuit().name().substring(0, 1).toLowerCase();
+    }
 }
